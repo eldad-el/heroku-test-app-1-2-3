cdcf08a47ae69eec94d255557e355da8badf2225
Eldad Livschitz <eldad.el1234@gmail.com>
2020-11-06 17:50:08 +0200

    app.py edit

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def index():
     return "<h1>Welcome to our server !!</h1>"
 
+@app.route("/home/")
+def home_page():
+    return "<h1>homepage</h1>"
+
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
