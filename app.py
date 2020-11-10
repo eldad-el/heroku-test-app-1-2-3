5a33cbec028dc0ae55e4fcff1aa65ef4b40ba87c
Eldad Livschitz <eldad.el1234@gmail.com>
2020-11-10 23:27:48 +0200

    commit_after_adding_ph_and_styles

--- app.py
+++ app.py
@@ -1,13 +1,9 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Welcome to our server !!</h1>"
-
-@app.route('/home/')
-def homepage():
-    return "<h1>homepage2</h1>"
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
---
+++ index.php
@@ -0,0 +1,61 @@
+<?php include 'inc/header.php'; ?>
+<br><br>
+
+        <label class="label-home">Latest News</label>
+        <br>
+        
+        <div class="contain-home">
+            <hr>         
+            <div class="latest">
+                <h3>Game 6 of the ECF highlights</h3>
+                <iframe margin-left="50px" width="727" height="409" src="https://www.youtube.com/embed/joQ75X2brJ8" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+            <hr>
+            <div class="latest">
+                <h3><a href="https://www.nba.com/bucks/features/boeder-10-most-memorable-regular-season-games-1819">10 Most Memorable Regular Season Games Of 2018–19</a></h3>
+            <a href="https://www.nba.com/bucks/features/boeder-10-most-memorable-regular-season-games-1819"><img class="pic-home" src="test.jpg" alt=""></a>
+            </div>
+            <hr>
+            <div class="latest">
+                <h3>Best Blocks Of The 2019 Playoffs</h3>
+                <iframe width="727" height="409" src="https://www.youtube.com/embed/DHyYnjpxt0Q" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+        </div>
+
+        <br><br>
+
+        <label class="label-home">Latest items on shop</label>
+        <br>
+        
+        <div class="contain-home">
+            <hr>
+            <div class="latest">
+                <img class="pic-items" src="items/glasses_1.jpg" alt="glasses 1">
+                <div style="display: block;">
+                    <label class="label-items">29.99$</label>
+                    <button class="btn-items"><a href="buy.php">purchase</a></button>
+                </div>
+            </div>
+            <div class="latest">
+                <img class="pic-items" src="items/hat_1.jpg" alt="hat 1">
+                <div style="display: block;">
+                    <label class="label-items">34.99$</label>
+                    <button class="btn-items"><a href="buy.php">purchase</a></button>
+                </div>
+            </div>
+            <hr>
+        </div>
+
+        <br><br>
+
+        <label class="label-home">Why The Bucks</label>
+        <div class="contain-home">
+            <p>The milwaukee bucks is a rising team in our time. This year, they had the best record in the league 
+                with 60 wins and 22 losses, and in the playoffs, they got to the Eastern Confrence Finals.
+                <br>
+                With the leader of the team, the Greek Freak, Giannis Antetokoumpo, along side Khris Middleton, 
+                Eric Bledsoe and Malcolm Brogdon, the team will be headed to one of their best years in the NBA.
+            </p>
+        </div>
+
+<?php include 'inc/footer.php'; ?>
