d4554b9498f41f1a911238035e95aac537b713af
Eldad Livschitz <eldad.el1234@gmail.com>
2020-11-10 23:35:32 +0200

    deleted _2 from index func in api.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index_2.html")
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
