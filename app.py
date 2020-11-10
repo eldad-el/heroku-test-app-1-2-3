b6fadb20a6b4706f66bd4ccefe68b26ed1989976
Eldad Livschitz <eldad.el1234@gmail.com>
2020-11-10 23:39:11 +0200

    reverted app.py index func to return str

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return "<h1>hi</h1>"
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
