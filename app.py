0ef0321dc3f2e618dcb043a6c623dcb08c9d3489
Eldad Livschitz <eldad.el1234@gmail.com>
2020-11-10 23:47:54 +0200

    added tempaltes folder and put index.html in that folder

--- app.py
+++ app.py
@@ -3,9 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return jsonify(username=test)
-
-
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
