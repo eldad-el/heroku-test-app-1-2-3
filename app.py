90caa2ece209446d9445574cba1186347f2372f9
Eldad Livschitz <eldad.el1234@gmail.com>
2020-11-11 22:28:15 +0200

    aded sqlalchemy, added db, added table to index

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, request, jsonify, render_template, url_for
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///test.db"
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(10000), nullable=False)
+    completed = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow())
+    
+    def __repr__(self):
+        return '<Task %r>' % self.id
+    
+    
 @app.route('/')
 def index():
     return render_template("index.html")
